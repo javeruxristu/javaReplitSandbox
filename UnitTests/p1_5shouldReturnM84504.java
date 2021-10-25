c208949e3f32b1b93eb1c4b43b5b4904e8245b21
javeruxristu <92879514+javeruxristu@users.noreply.github.com>
2021-10-25 23:47:52 +0300

    Create p1_5shouldReturnM84504.java

---
+++ UnitTests/p1_5shouldReturnM84504.java
@@ -0,0 +1,9 @@
+@Test
+public void p1_5shouldReturnM84504() {
+  // Enter code here
+    // Enter code here
+Calc c = new Calc();
+  double t = c.sumVirkni(1.5);
+        assertEquals(-84504.17362949256, t, 0.0000000000001);  // Enter code here
+  
+}
