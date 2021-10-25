7d2ac0fb29f1c8300920552f8a06b6ed3a308ae1
javeruxristu <92879514+javeruxristu@users.noreply.github.com>
2021-10-25 23:48:15 +0300

    Create p1_shouldReturn1066.java

---
+++ UnitTests/p1_shouldReturn1066.java
@@ -0,0 +1,8 @@
+@Test
+public void p1_shouldReturn1066() {
+  // Enter code here
+    // Enter code here
+Calc c = new Calc();
+  double t = c.sumVirkni(1);
+        assertEquals(-4920.666666666667, t, 0.0000000000001);
+}
